fe47b02852a561275770bcc446cb533097d45239
michael <mchege78@gmail.com>
2023-01-20 18:23:11 +0300

    Added react-testing

---
+++ react-testing/src/App.test.js
@@ -0,0 +1,46 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import FeedbackForm from "./FeedbackForm";
+
+describe("Feedback Form", () => {
+  test("User is able to submit the form if the score is lower than 5 and additional feedback is provided", () => {
+    const score = "3";
+    const comment = "The pizza crust was too thick";
+
+    // Create a mock function that will capture calls to the function & parameters passed to the calls
+    const handleSubmit = jest.fn();
+    render(<FeedbackForm onSubmit={handleSubmit} />);
+
+    // Access the range input value and change it's value
+    const rangeInput = screen.getByLabelText(/Score:/);
+    fireEvent.change(rangeInput, { target: { value: `${score}` } });
+
+    const commentArea = screen.getByLabelText(/Comments:/);
+    fireEvent.change(commentArea, { target: { value: `${comment}` } });
+
+    // Access the sumbit button and click it
+    const submitButton = screen.getByRole("button");
+    fireEvent.click(submitButton);
+
+    expect(handleSubmit).toHaveBeenCalledWith({
+      score,
+      comment,
+    });
+  });
+
+  test("User is able to submit the form if the score is higher than 5, without additional feedback", () => {
+    const score = "9";
+    const handleSubmit = jest.fn();
+    render(<FeedbackForm onSubmit={handleSubmit} />);
+
+    const rangeInput = screen.getByLabelText(/Score:/);
+    fireEvent.change(rangeInput, { target: { value: `${score}` } });
+
+    const submitButton = screen.getByRole("button");
+    fireEvent.click(submitButton);
+
+    expect(handleSubmit).toHaveBeenCalledWith({
+      score,
+      comment: "",
+    });
+  });
+});
